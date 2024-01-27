57797b9804b071c17119e2fd613146433d0c5356
NRG-Drink <beatdz93@hotmail.com>
2024-01-27 18:47:52 +0100

    chore: change laptop config

--- vars.php
+++ vars.php
@@ -1,7 +1,21 @@
 <?php
-// DB Connection.
-$host = "192.168.1.200";
-$user = "dbuser";
-$password = "password";
-$database = "mydb";
-$table = "mytable";
+// DB Connection. (laptop)
+$host = "192.168.1.201";
+$user = "php";
+$password = "paSSw0rd!";
+$database = "throw_away_tasklist";
+$table = "tasklist1";
+
+// DB Connection. (pc)
+// $host = "192.168.1.200";
+// $user = "dbuser";
+// $password = "password";
+// $database = "mydb";
+// $table = "mytable";
+
+// DB Connection. (pc2)
+// $host = "192.168.1.199";
+// $user = "php";
+// $password = "password";
+// $database = "db";
+// $table = "tbl";
